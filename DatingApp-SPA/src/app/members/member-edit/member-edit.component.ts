d6e38d5e5eb5ecc8f0e3493918a02f6e542f2900
Pankaj Rayal <Pankaj Rayal>
2019-11-28 21:01:31 +0530

    Section 11 Complete

--- DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
+++ DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -13,6 +13,7 @@ import { AuthService } from 'src/app/_services/auth.service';
 })
 export class MemberEditComponent implements OnInit {
   user: User;
+  photoUrl:string;
   @ViewChild('editForm', { static: false }) editForm: NgForm;
   @HostListener('window:beforeunload', ['$event'])
   unloadNotification($event: any) {
@@ -31,6 +32,7 @@ export class MemberEditComponent implements OnInit {
     this.route.data.subscribe(data => {
       this.user = data['user'];
     });
+    this.authService.currentPhotoUrl.subscribe(photoUrl=> this.photoUrl = photoUrl);
   }
 
   updateUser() {
@@ -45,5 +47,8 @@ export class MemberEditComponent implements OnInit {
           this.alertify.error(error);
         }
       );
+  }
+  updateMainPhoto(photoUrl){
+    this.user.photoUrl = photoUrl;
   }
 }
