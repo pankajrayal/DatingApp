67c7ae52326dd8144bd850181027f11edfaac4f6
Pankaj Rayal <Pankaj Rayal>
2019-12-10 20:39:18 +0530

    Section 14 Completed

--- DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
+++ DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -14,6 +14,7 @@ import { AuthService } from 'src/app/_services/auth.service';
 export class MemberEditComponent implements OnInit {
   user: User;
   photoUrl:string;
+
   @ViewChild('editForm', { static: false }) editForm: NgForm;
   @HostListener('window:beforeunload', ['$event'])
   unloadNotification($event: any) {
